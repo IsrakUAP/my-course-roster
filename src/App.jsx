39c7b11cacb711472557c73066237993eddc8823
IsrakUAP <israk2301@gmail.com>
2023-09-15 04:19:35 +0600

    fetching JSON data inside the child component cards.jsx , implement layout inside another child component card.jsx passing data where app.jsx is a parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
+import Cards from './components/Cards/Cards'
+import Cart from './components/Cart/Cart';
+
 
 function App() {
+  
 
   return (
     <>
-      <h1>Vite + React</h1>
-    
+      <div className='headerContainer'>
+        <Cards></Cards>
+        <Cart></Cart>
+      </div>
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+
+
+const Card = ({card}) => {
+    const {courseImg,courseTitle,courseDescription,courseFeeIcon,courseFee,courseIcon,courseCredit} = card;
+    console.log(card);
+    return (
+        <div>
+            <img src={courseImg} alt="" />
+            <h3>{courseTitle}</h3>
+            <p>{courseDescription}</p>
+            <div>
+                <img src={courseFeeIcon} alt="" /><span>Price: {courseFee}</span> <img src={courseIcon} alt="" /><span>Credit: {courseCredit} </span>
+            </div>
+            <button>Select</button>
+        </div>
+    );
+};
+
+export default Card;
