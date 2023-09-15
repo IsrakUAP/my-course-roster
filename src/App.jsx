68ba2558ab790685c06191cb76d93f08e134ddb7
IsrakUAP <israk2301@gmail.com>
2023-09-15 10:27:28 +0600

    executed react-toastify for showing the error message that per course can be included for one time

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Cards from './components/Cards/Cards'
 import Cart from './components/Cart/Cart';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -11,11 +13,20 @@ function App() {
     const isExist = coursename.find(data => data.courseTitle == card.courseTitle)
 
     if (isExist) {
-      alert('This course is already added in the list')
-  
+     return toast.error('This course is already added in the list', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
     }
     else{
       setCoursename([...coursename, card]);
+      return;
     }
   }
   
@@ -26,6 +37,7 @@ function App() {
         <Cards countCourse={countCourse}></Cards>
         <Cart coursename={coursename}></Cart>
       </div>
+      <ToastContainer/>
     </>
   )
 }
